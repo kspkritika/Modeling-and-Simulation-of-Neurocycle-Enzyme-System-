8ae2707355b335634347e9c35d1046c452952f51
kspkritika <35979747+kspkritika@users.noreply.github.com>
2018-03-03 21:02:38 -0500

    The code

---
+++ neurocycle.m
@@ -0,0 +1,7 @@
+function [t, y] = neurocycle(hf,Tend,y0,S,standard)
+%if nargin == 2, 
+%S = 'b'; standard = 1; end % setting defaults
+%if nargin == 3, S = 'b'; standard = 1; end
+%if nargin == 4, standard = 1; end
+
+
